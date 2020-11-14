1517513c025309a695cead54072c6eec9867c2c9
Stanley Dharan <s-dharan@hotmail.com>
2020-11-14 16:51:37 -0500

    Adding styling to nav to sperate from lower content

--- src/components/NavigationBar/Nav.js
+++ src/components/NavigationBar/Nav.js
@@ -2,11 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { Menu } from "antd";
 import {Landing, NewClient, ClientList} from "../../pages"
+import "./nav.css"
 
 const Nav = () => {
   return (
     <Router>
-      <Menu theme="light" mode="horizontal">
+      <Menu theme="light" mode="horizontal" id="white-space-bottom">
         <Menu.Item>
           <Link to="/">Home</Link>
         </Menu.Item>
