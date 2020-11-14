1c015f657eb899bf95aa74e6a9bcc25eeac0e892
Stanley Dharan <s-dharan@hotmail.com>
2020-11-14 13:39:39 -0500

    Setting up Client context

---
+++ src/context/client-context/client-context.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const clientObject = {
+   firstName: "",
+   lastName: "",
+   middleName: "",
+   forwardAddressLine: "",
+   authorizedPickupFirstName: "",
+   authorizedPickupLastName: "",
+   profile_image_id: "",
+   totalMail: 0,
+   pendingMail: 0
+};
+
+const ClientContext = React.createContext(clientObject);
+export default ClientContext;
