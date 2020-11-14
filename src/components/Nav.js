e2ee8e4743de3e225427589b06f52dd69b59be81
Stanley Dharan <s-dharan@hotmail.com>
2020-11-13 22:55:08 -0500

    Routing and navigation bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Menu } from "antd";
+import '../App.css';
 
 const Nav = () => {
   return (
-    <Menu theme="light" mode="horizontal" style={{ borderBottom: 0 }}>
-      <Menu.Item>
-        <Link to="/login">Nav!</Link>
-      </Menu.Item>
-    </Menu>
+    <Router>
+      <Menu theme="light" mode="horizontal">
+        <Menu.Item>
+          <Route exact path="/">
+            <Link to="/search">Home</Link>
+          </Route>
+        </Menu.Item>
+        <Menu.Item>
+          <Route exact path="/search">
+            <Link to="/search">Search</Link>
+          </Route>
+        </Menu.Item>
+      </Menu>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 // import App from "./components/app/App";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
