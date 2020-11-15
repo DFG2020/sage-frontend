df202e3e0905e20bddec0786115de50d9deb4bd2
Stanley Dharan <s-dharan@hotmail.com>
2020-11-15 16:08:14 -0500

    mail results page has update buttongit statusgit status

--- src/pages/mail-results/MailResults.js
+++ src/pages/mail-results/MailResults.js
@@ -1,64 +1,15 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Layout, Table, Button, Modal } from "antd";
 import axios from "axios";
-// import moment from "moment";
+import moment from "moment";
 import { PlusOutlined } from "@ant-design/icons";
-import AddMail from "../../components/modals/add-mail/AddMail";
+import {AddMail, UpdateMail} from "../../components/modals";
 import { ClientIdContext } from "../../context"
 import "./MailResults.css";
 
-const columns = [
-  {
-    title: "Sender",
-    dataIndex: "fulfillmentProvider",
-    key: "fulfillmentProvider",
-  },
-  {
-    title: "Comments",
-    dataIndex: "comment",
-    key: "comments",
-  },
-  {
-    title: "Staff Initials",
-    dataIndex: "staffInitial",
-    key: "staffInitial",
-  },
-  {
-    title: "Status of Package",
-    dataIndex: "status",
-    key: "status",
-  },
-  {
-    title: "Received Date",
-    dataIndex: "receivedDateTimeMs",
-    key: "receivedDateTimeMs",
-  },
-  {
-    title: "Type of Package",
-    dataIndex: "type",
-    key: "type",
-  },
-  {
-    title: "Pick up Signature",
-    dataIndex: "signatureImageId",
-    key: "signatureImageId",
-  },
-  {
-    title: "Date picked Up",
-    dataIndex: "pickedUpDateTimeMs",
-    key: "pickedUpDateTimeMs",
-  },
-  {
-    title: "Action",
-    key: "operation",
-    fixed: "right",
-    width: 100,
-    render: () => <p>Update</p>, // open modal on click
-  },
-];
-
 const MailResults = () => {
   const [newMailModalOpen, setNewMailModalOpen] = useState(false);
+  const [updateMailModalOpen, setupdateMailModalOpen] = useState(false);
   // eslint-disable-next-line no-unused-vars
   const [mailList, setMailList] = useState([]);
   // eslint-disable-next-line no-unused-vars
@@ -71,7 +22,14 @@ const MailResults = () => {
     axios.get(`http://localhost:8000/api/mail/user/${clientId}`, {})
     .then((response) => {
       for(let i = 0; i < response.data.length; i += 1){
-        mailObjectList.push(response.data[i]?.mail);
+        const mail = response.data[i]?.mail;
+        const mailRecievedMS = response.data[i]?.mail.receivedDateTimeMs;
+        const mailPickedUpMS = response.data[i]?.mail.pickedUpDateTimeMs;
+
+        mail.pickedUpDateTimeMs = mailPickedUpMS ? moment(mailPickedUpMS).format("DD/MM/YYYY") : ""
+
+        mail.receivedDateTimeMs = moment(mailRecievedMS).format("DD/MM/YYYY");
+        mailObjectList.push(mail);
         if(i === response.data.length - 1){
           setDataLoading(false)
           setMailList(mailObjectList);
@@ -80,20 +38,9 @@ const MailResults = () => {
     });
   }, [dataLoading]);
 
-  // const convertMSTimeToDate = () => {
-  //   // const convertedMailList = [];
-  //   console.log(mailList);
-  //   for(let i = 0; i <= mailList.length; i += 1) {
-  //     const copiedMailItem = mailList[i];
-  //     const mailRecievedMS = copiedMailItem.receivedDateTimeMs;
-  //     copiedMailItem.receivedDateTimeMs = moment(mailRecievedMS).format("DD MM YYYY");
-  //     console.log(copiedMailItem.receivedDateTimeMs);
-  //     // if(copiedMailItem.pickedUpDateTimeMs){
-
-  //     // }
-  //   }
-  //   return mailList
-  // }
+  const updateMail = () => {
+    setupdateMailModalOpen(true);
+  };
 
   const name = "LeBron James"; // to be fetched from the backend
 
@@ -105,12 +52,90 @@ const MailResults = () => {
     // send api call to create new mail. Then add it to the table
     console.log(e);
     setNewMailModalOpen(false);
+    setupdateMailModalOpen(false);
   };
 
   const handleCancel = () => {
     // Do nothing
     setNewMailModalOpen(false);
+    setupdateMailModalOpen(false);
   };
+
+  const updateButton = () => {
+    return (
+      <>
+        <Button
+          icon={<PlusOutlined />}
+          type="primary"
+          className="newMailButton"
+          onClick={updateMail}
+        >
+          Update
+        </Button>
+
+        <Modal
+          title="Update Mail"
+          visible={updateMailModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          width="70rem"
+          okButtonProps={{form:'addMailForm', key: 'submit', htmlType: 'submit'}}
+        >
+          <UpdateMail />
+        </Modal>
+      </>
+    )
+  }
+
+  const columns = [
+    {
+      title: "Sender",
+      dataIndex: "fulfillmentProvider",
+      key: "fulfillmentProvider",
+    },
+    {
+      title: "Comments",
+      dataIndex: "comment",
+      key: "comments",
+    },
+    {
+      title: "Staff Initials",
+      dataIndex: "staffInitial",
+      key: "staffInitial",
+    },
+    {
+      title: "Status of Package",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "Received Date",
+      dataIndex: "receivedDateTimeMs",
+      key: "receivedDateTimeMs",
+    },
+    {
+      title: "Type of Package",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "Pick up Signature",
+      dataIndex: "signatureImageId",
+      key: "signatureImageId",
+    },
+    {
+      title: "Date picked Up",
+      dataIndex: "pickedUpDateTimeMs",
+      key: "pickedUpDateTimeMs",
+    },
+    {
+      title: "Action",
+      key: "operation",
+      fixed: "right",
+      width: 100,
+      render: () => updateButton()
+    },
+  ];
 
   return (
     <Layout className="layout" style={{ background: "white" }}>
